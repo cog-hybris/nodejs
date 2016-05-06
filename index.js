55c380e7a9405bccc51a592f69129d820238c612
ajay kamble <ajay.kamble@cognizant.com>
2016-05-06 04:45:04 +0000

    updated index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send(' test  Applicaiton\n');
+  res.send(' test nodejs Applicaiton\n');
 });
 
 app.listen(PORT);
